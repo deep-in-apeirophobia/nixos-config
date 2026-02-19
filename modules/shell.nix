54e4c51b9375f8966c04295836eed61172187758
Atrin Hojjat <atrin.hojjat@gmail.com>
2026-02-18 23:06:06 -0500

    Add starship config

--- modules/shell.nix
+++ modules/shell.nix
@@ -11,7 +11,28 @@
 			cat = "bat --style=plain --paging=never";
 		};
 		interactiveShellInit = ''
+			fish_vi_key_bindings
 			set -g fish_greeting
+			starship init fish | source
 		'';
 	};
+
+	home.file.".config/starship.toml".text = ''
+# Spacefish-inspired prompt via Starship
+format = "$all"
+
+[character]
+success_symbol = "❯"
+error_symbol = "✖"
+
+[command_duration]
+min_time = 200
+show_milliseconds = true
+format = "took [$duration]($style) "
+
+[time]
+disabled = false
+time_format = "%H:%M:%S"
+format = "[$time]($style) "
+'';
 }
