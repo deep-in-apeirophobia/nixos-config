187621702fccbf873a98eebe03e7956f13e863b1
Atrin Hojjat <atrin.hojjat@gmail.com>
2026-02-19 08:12:07 -0500

    Add proxy config with clashverge

---
+++ modules/proxies.nix
@@ -0,0 +1,10 @@
+{ nixpkgs, conf, lib, pkgs, hostname, username, ...}: 
+{
+	programs.clash-verge = {
+    enable = true;
+    serviceMode = true;
+    tunMode = true;
+    autoStart = false;
+    # package = pkgs.clash-nyanpasu; # Alternative fork
+  };
+}
