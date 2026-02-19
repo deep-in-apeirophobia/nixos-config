44ad72f9c96ca62aa519b71a66f19d4323dc223b
Atrin Hojjat <atrin.hojjat@gmail.com>
2026-02-18 22:44:03 -0500

    Use pretty outputs for default shell commands

--- modules/shell.nix
+++ modules/shell.nix
@@ -6,6 +6,10 @@
 
 	programs.fish = {
 		enable = true;
+		shellAliases = {
+			ls = "eza --group-directories-first --icons=auto --color=auto";
+			cat = "bat --style=plain --paging=never";
+		};
 		interactiveShellInit = ''
 			set -g fish_greeting
 		'';
