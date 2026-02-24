2b2f905be39767d90d15276ae3d6a9a055808aef
Atrin Hojjat <atrin.hojjat@gmail.com>
2026-02-24 17:52:46 -0500

    Update hyprland config

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -19,11 +19,34 @@
 		"$mod" = "SUPER";
 		bind = [
 			"$mod, SPACE, exec, wofi --show drun"
+
 			"$mod, h, movefocus, l"
 			"$mod, j, movefocus, d"
 			"$mod, k, movefocus, u"
 			"$mod, l, movefocus, r"
+
+			(builtins.concatLists (builtins.genList (x: 
+				let 
+					ws = builtins.toString (x + 1);
+					key = if x == 9 then "0" else builtins.toString (x + 1);
+				in [
+					"$mod, ${key}, workspace, ${ws}"
+					"$mod SHIFT, ${key}, movetoworkspace, ${ws}"
+					"$mod CTRL, ${key}, movetoworkspacesilent, ${ws}"
+				]
+			) 10));
+			"$mod, S, togglespecialworkspace, scratchpad"
+			"$mod SHIFT, S, movetoworkspace, special:scratchpad"
+
+			"$mod, Tab, workspace, m+1"  # Next workspace
+			"$mod SHIFT, Tab, workspace, m-1"  # Previous workspace
+
 			"$mod, backslash, togglefloating,"
+			"$mod, Return, exec, kitty"
+			"$mod, Q, killactive,"
+			"$mod, F, fullscreen,"
+
+
 			"SUPER, S, exec, grim -g \"$(slurp)\" - | swappy -f -"
 		];
 	};
