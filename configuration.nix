5afc92dade695f573a2bdb134ec8fc83e470159b
Atrin Hojjat <atrin.hojjat@gmail.com>
2026-02-24 18:37:16 -0500

    Add experimental features

--- configuration.nix
+++ configuration.nix
@@ -121,6 +121,7 @@
 	};
 
 	nixpkgs.config.chromium.commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland";
+	nix.settings.experimental-features = "nix-command flakes"
 
 	programs.firefox = {
     enable = true;
