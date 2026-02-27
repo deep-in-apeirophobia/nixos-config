4df001e47b7fc972e1e0f7b452f5949cba5383c8
Atrin Hojjat <atrin.hojjat@gmail.com>
2026-02-27 16:37:05 +0000

    Add services dir

---
+++ services/tor.nix
@@ -0,0 +1,32 @@
+{ nixpkgs, conf, config, lib, pkgs, hostname, username, ...}: 
+{
+	environment.systemPackages = with pkgs; [
+		tor
+		obfs4
+		webtunnel
+		nyx
+	];
+	
+	services.tor = {
+		enable = true;
+		openFirewall = true;
+		# relay = {
+		# 	enable = true;
+		# 	role = "relay";
+		# };
+		client.enable = true;
+		settings = {
+			SocksPort = 9050;
+			ControlPort = 9051;
+
+			# UseBridges = true;
+			# ClientTransportPlugin = "obfs4 exec ${pkgs.obfs4}/bin/lyrebird";
+			# Bridge = "obfs4 IP:ORPort [fingerprint]";
+
+			# ContactInfo = "whoknows";
+			# Nickname = "idk";
+			# ORPort = 9001;
+			# BandWidthRate = "1 MBytes";
+		};
+	};
+}
---
+++ services/udisk2.nix
@@ -0,0 +1,9 @@
+{ nixpkgs, conf, config, lib, pkgs, hostname, username, ...}: 
+{
+	
+	services.udisk2 = {
+		enable = true;
+		mountOnMedia = true;
+	};
+
+}
