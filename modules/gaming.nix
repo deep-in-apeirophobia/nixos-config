da7424fa6ebeb7979408674c112f5831cd08ef8a
Atrin Hojjat <atrin.hojjat@gmail.com>
2026-03-13 13:00:13 +0000

    Add airshipper

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -1,23 +1,13 @@
 { config, pkgs, ... }:
 {
-	programs.steam = {
-		enable = true;
-		remotePlay.openFirewall = true;
-		dedicatedServer.openFirewall = true;
-	};
 
-	hardware.opengl = {
-		enable = true;
-		driSupport = true;
-		driSupport32Bit = true;
-	};
-
-	environment.systemPackages = with pkgs; [
-		steam
-		steam-run
-		vulkan-tools
-		vulkan-loader
-		vulkan-validation-layers
-		godot_4
+	home.packages = with pkgs; [
+		# steam
+		# steam-run
+		# vulkan-tools
+		# vulkan-loader
+		# vulkan-validation-layers
+		airshipper
+		# godot_4
 	];
 }
