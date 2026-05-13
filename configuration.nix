e50f712a83211021c4c0e3c6538a728487334e51
Atrin Hojjat <atrin.hojjat@gmail.com>
2026-05-13 14:42:49 +0000

    Disable garbage collection due to crappy internet

--- configuration.nix
+++ configuration.nix
@@ -56,9 +56,9 @@
 		automatic = false;
 	};
 	nix.gc = {
-		automatic = true;
+		automatic = false;
 		dates = "weekly";
-		options = "--delete-older-than 30d";
+		options = "--delete-older-than 90d";
 	};
 
 	###################
