3a59eb4f57069633d7f831bdb6bc1d6ea484b7fc
Atrin Hojjat <atrin.hojjat@gmail.com>
2026-05-14 18:22:06 +0000

    TEMP: Fix gotools install fix: NixOS/nixpkgs#509480

--- modules/dev/go.nix
+++ modules/dev/go.nix
@@ -1,9 +1,18 @@
-{ config, pkgs, ... }:
+{ config, pkgs, ... }: let
+  gotoolsWithoutModernize = pkgs.symlinkJoin {
+    name = "gotools-without-modernize";
+    paths = [pkgs.gotools];
+    postBuild = ''
+      rm -f "$out/bin/modernize"
+    '';
+  };
+in
 {
 	home.packages = with pkgs; [
 		go
 		gopls
-		gotools
+		# gotools
+		gotoolsWithoutModernize
 		golangci-lint
 		delve
 		govulncheck
